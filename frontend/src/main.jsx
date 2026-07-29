60ac9f0d38b4b5b5f551ba2957a18297b3b9b218
Obaid-09 <hassaanobaid011@gmail.com>
2026-07-30 00:15:40 +0530

    Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 
-import { store } from "./redux/store";
+import { store } from "./redux/store.js";
 import ToastProvider from "./components/ui/ToastProvider"
 import { AuthProvider } from "./context/AuthContext";
 
